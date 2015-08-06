07bc4c2617d4c62f616c97022dc279f17265531b
nicov <n.villalonga@globant.com>
2015-08-06 11:04:05 -0300

    show button assign && process

--- app/scripts/schedule-display/services/constants-helper.js
+++ app/scripts/schedule-display/services/constants-helper.js
@@ -22,6 +22,10 @@ var constants = {
         end : 20
     },
 
+    "actualSelection" : {},
+
+    "selectedFlights" : [],
+
     "taskNames" : [
         "Tail#1 Flight", "Tail#2 Flight", "Tail#3 Flight", "Tail#4 Flight", "Tail#5 Flight"
     ],    
@@ -69,3 +73,107 @@ constants.y = d3.scale.ordinal().domain(constants.taskTypes).rangeRoundBands([ 0
 constants.xAxis = d3.svg.axis().scale(constants.x).orient("bottom").tickFormat(d3.time.format(constants.tickFormat)).tickSubdivide(true).tickSize(5).tickPadding(3);
 
 constants.yAxis = d3.svg.axis().scale(constants.y).orient("left").tickSize(0);
+
+constants.service = {
+
+    showButtonAssign : function(d) {
+        if(!d)
+            return false;
+
+        var buttonAssign = $('.button-assign'),
+            buttonProgress = $('.button-progress');
+
+        constants.elementSpacing.top = constants.y(d.taskName) + 25;
+        constants.elementSpacing.start = constants.x(d.startDate) + 70;
+        constants.elementSpacing.end = constants.x(d.endDate) + 70;
+
+        constants.actualSelection = d;
+
+        constants.service.positionButton(buttonAssign);
+        constants.service.positionButton(buttonProgress);
+
+        if(!constants.service.flightIsAssigned(d)) {
+            constants.service.hideElement(buttonProgress);
+            constants.service.showElement(buttonAssign);
+        } else {
+            constants.service.hideElement(buttonAssign);          
+            constants.service.showElement(buttonProgress);
+        }
+
+    },
+
+    hideButtonAssign : function() {
+        var buttonAssign = $('.button-assign');
+
+        constants.service.hideElement(buttonAssign);
+    },
+
+    showButtonProgress : function() {
+        var buttonAssign = $('.button-assign'),
+            buttonProgress = $('.button-progress');
+        
+        constants.service.hideElement(buttonAssign);
+        constants.service.positionButton(buttonProgress);
+        constants.service.showElement(buttonProgress);
+    },
+
+
+
+    hideButtonProgress : function() {
+        var buttonProgress = $('.button-progress');
+
+        constants.service.hideElement(buttonProgress);
+    },
+
+    positionButton : function(button) {
+        button.css("left", constants.elementSpacing.start)
+            .css("top", constants.elementSpacing.top);
+    },
+
+    showElement : function(element) {
+        element.removeClass('hide-element')
+            .addClass('show-element');
+    },
+
+
+
+    hideElement : function(element) {
+        element.removeClass('show-element')
+            .addClass('hide-element');
+    },
+
+    assignFlight : function() {
+        console.log("asd");
+        constants.actualSelection.statusAlert = "pending";
+
+        var flight = constants.actualSelection;
+
+        constants.selectedFlights.push(flight);
+        //constants.service.showLogo(flight.statusAlert, constants.selectedFlights.length-1);
+
+        constants.service.processFlight();
+
+        //constants.service.drawLogo();
+    },
+
+
+    processFlight : function () {
+        constants.service.hideButtonAssign();
+        constants.service.showButtonProgress();
+    },
+
+
+    flightIsAssigned : function(d) {
+        var length = constants.selectedFlights.length;
+
+        for(var i = 0; i < length; i++) {
+            if(constants.selectedFlights[i].startDate === d.startDate)
+                return true
+        }
+
+        return false;
+    }
+
+
+
+};
