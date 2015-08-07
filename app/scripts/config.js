a6482f079613832217c86f2fe64975f7ad315b73
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-08-07 13:14:08 -0500

    Add nav bar

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 /* jshint quotmark:true, indent:false, white: false */
-// This file was generated at build time on Thu Aug 06 2015 11:05:24 GMT-0500 (CDT)
+// This file was generated at build time on Thu Aug 06 2015 15:56:20 GMT-0500 (CDT)
 // Any change here will be lost
 
 'use strict';
