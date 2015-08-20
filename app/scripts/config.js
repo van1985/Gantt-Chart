6c79b7f3a8ea2c5a521db2f73db6b4f4cc222329
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-08-20 11:42:52 -0500

    Modify Flight.json

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 /* jshint quotmark:true, indent:false, white: false */
-// This file was generated at build time on Wed Aug 19 2015 15:21:01 GMT-0500 (CDT)
+// This file was generated at build time on Thu Aug 20 2015 11:31:42 GMT-0500 (CDT)
 // Any change here will be lost
 
 'use strict';
