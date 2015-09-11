180bbebb44e0e74f19a4af107455df6de0001942
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-09-11 10:36:58 -0300

    add solution list

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 /* jshint quotmark:true, indent:false, white: false */
-// This file was generated at build time on Thu Sep 10 2015 11:50:59 GMT-0300 (ART)
+// This file was generated at build time on Thu Sep 10 2015 18:54:18 GMT-0300 (ART)
 // Any change here will be lost
 
 'use strict';
