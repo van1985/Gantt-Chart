7454be0cad31daf8bfb503f8b4a3dc16d5d27f6b
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-09-15 00:46:32 -0300

    Add more results for solution-list

--- app/scripts/solutions-list/controllers/solutionlist.js
+++ app/scripts/solutions-list/controllers/solutionlist.js
@@ -1,8 +1,35 @@
 'use strict';
 
-angular.module('SolutionList').controller('solutionListCtrl', function () {
+angular.module('SolutionList').controller('solutionListCtrl', function ($scope) {
 
 console.log("solution list");
 
+$scope.showMoreLineHoldersButton = true;
+$scope.moreLineHolders = function(){
+	if ($scope.showMoreLineHoldersButton)
+	{
+		$('#collapseLineHolders').slideDown('slow');
+		$scope.showMoreLineHoldersButton = false;
+	}
+	else
+	{
+		$('#collapseLineHolders').slideUp('slow');
+		$scope.showMoreLineHoldersButton = true;	
+	}
+}
+
+$scope.showMoreReservesButton = true;
+$scope.moreReserve = function(){
+	if ($scope.showMoreReservesButton)
+	{
+		$('#collapseReserves').slideDown('slow');
+		$scope.showMoreReservesButton = false;
+	}
+	else
+	{
+		$('#collapseReserves').slideUp('slow');
+		$scope.showMoreReservesButton = true;	
+	}
+}
 
 });
