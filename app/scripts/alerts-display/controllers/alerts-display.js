86855a2e9b782c71d781be208740bbddc9018779
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-09-16 15:54:14 -0300

    add menu popup

--- app/scripts/alerts-display/controllers/alerts-display.js
+++ app/scripts/alerts-display/controllers/alerts-display.js
@@ -1,7 +1,12 @@
 'use strict';
 
-angular.module('AlertsDisplay').controller('alertsDisplayCtrl', function ($scope) {
+angular.module('AlertsDisplay').controller('alertsDisplayCtrl', function ($scope, $location) {
 
 $scope.name = 'Alerts display';
 
+$scope.navigate = function (view) {
+	if (view === 'view solutions'){
+		$location.path('/solutions-list');
+	}
+}
 });
--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 /* jshint quotmark:true, indent:false, white: false */
-// This file was generated at build time on Wed Sep 16 2015 00:21:33 GMT-0300 (ART)
+// This file was generated at build time on Wed Sep 16 2015 15:43:23 GMT-0300 (ART)
 // Any change here will be lost
 
 'use strict';
